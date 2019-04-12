b40ba8a87b3ebc86e12ad5a5d0ba60278f0b7a4a
Krzysztof <krzysztof211@gmail.com>
2019-04-12 21:53:59 +0200

    categories dictionary

---
+++ machineLearning/dataset.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 12 21:43:37 2019
+
+@author: Krzysztof Pasiewicz
+"""
+categories_dict = {
+        "myszki": "",
+        "klawiatury": "",
+        "zestaw klawiatura i mysz": "",
+        "pady": "",
+        "piloty": "",
+        "joysticki": "",
+        "tablety graficzne": "",
+        "monitory": "",
+        "dyski zewnetrzne i przenośne": "",
+        "pendrive": "",
+        "śledzie": "",
+        "kamery internetowe": "",
+        "zestawy i kamery do wideokonferencji": "",
+        "gogle VR": "",
+        "głośniki": "",
+        "mikrofony i słuchawki": ""
+        }
