c206f80735d2b23d1590e8e522bba0442588d05b
Zofia Tarant <zosiarrf@gmail.com>
2019-04-12 23:08:14 +0200

    Update authorization.py

--- alleCebula/alleCebula/authorization.py
+++ alleCebula/alleCebula/authorization.py
@@ -1,6 +1,10 @@
 from credentials import client_id, client_secret
 from base64 import b64encode
+from http.server import BaseHTTPRequestHandler, HTTPServer
 import requests
+import webbrowser
+
+REDIRECT_URI="http://localhost:5500"
 
 
 def get_access_token(client_id, client_secret):
@@ -15,4 +19,31 @@ def get_access_token(client_id, client_secret):
     return access_token
 
 
+def authorize_user(client_id, redirect_uri = REDIRECT_URI):
+
+    class Handler(BaseHTTPRequestHandler):
+        def __init__(self, request, address, server):
+            super().__init__(request, address, server)
+
+        def do_GET(self):
+            self.send_response(200, 'OK')
+            self.send_header('Content-Type', 'text/html')
+            self.end_headers()
+            self.server.path = self.path
+            self.server.code = self.path.rsplit('?code=', 1)[-1]
+
+    auth_url="https://allegro.pl/auth/oauth/authorize" \
+             "?response_type=code&client_id={}&redirect_url={}"\
+        .format(client_id, redirect_uri)
+
+    webbrowser.open(auth_url)
+
+    redirect_uri = requests.utils.urlparse(redirect_uri)
+    httpd = HTTPServer((redirect_uri.hostname, redirect_uri.port), Handler)
+    httpd.handle_request()
+    httpd.server_close()
+    return httpd.code
+
+
 token = get_access_token(client_id, client_secret)
+user_code = authorize_user(client_id)
