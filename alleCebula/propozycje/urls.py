082db42579807f0cb128f953ae3c67a863d8a4ce
unknown <miki.franek@gmail.com>
2019-04-12 22:17:27 +0200

    Categories

--- alleCebula/propozycje/urls.py
+++ alleCebula/propozycje/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.propositions, name='propositions'),
+    path('process/', views.processing, name='propositions-processing'),
 ]
--- machineLearning/dataset.py
+++ machineLearning/dataset.py
@@ -5,20 +5,20 @@ Created on Fri Apr 12 21:43:37 2019
 @author: Krzysztof Pasiewicz
 """
 categories_dict = {
-        "myszki": "",
-        "klawiatury": "",
-        "zestaw klawiatura i mysz": "",
-        "pady": "",
-        "piloty": "",
-        "joysticki": "",
-        "tablety graficzne": "",
-        "monitory": "",
-        "dyski zewnetrzne i przenośne": "",
-        "pendrive": "",
-        "śledzie": "",
-        "kamery internetowe": "",
-        "zestawy i kamery do wideokonferencji": "",
-        "gogle VR": "",
-        "głośniki": "",
-        "mikrofony i słuchawki": ""
+        "myszki": "4575",
+        "klawiatury": "4566",
+        "zestaw klawiatura i mysz": "9189",
+        "pady": "4569",
+        "piloty": "16641",
+        "joysticki": "4565",
+        "tablety graficzne": "4570",
+        "monitory": "260041",
+        "dyski zewnetrzne i przenośne": "77939",
+        "pendrive": "257960",
+        "śledzie": "4739",
+        "kamery internetowe": "260021",
+        "zestawy i kamery do wideokonferencji": "260023",
+        "gogle VR": "260043",
+        "głośniki": "259434",
+        "mikrofony i słuchawki": "259422"
         }
