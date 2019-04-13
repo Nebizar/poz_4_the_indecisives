f333568afdce7510657d07d84f769e59b673ead6
Krzysztof <krzysztof211@gmail.com>
2019-04-13 06:31:57 +0200

    db class added

--- alleCebula/deals/models.py
+++ alleCebula/deals/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Item(models.Model):
+    picture = models.TextField()
+    name = models.TextField()
+    price = models.CharField(max_length = 7)
+    cebulions = models.IntegerField()
+    category_id = models.CharField(max_length = 10)
