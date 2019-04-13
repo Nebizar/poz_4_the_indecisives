f175369ea76e62da0573970252089594de396b64
Zofia Tarant <zosiarrf@gmail.com>
2019-04-13 02:42:44 +0200

    Revert "update itemy"
    This reverts commit 177e67daeaf5a67ff1f9dedea38041810fa5ae84.

--- alleCebula/alleCebula/itemyZosi.py
+++ alleCebula/alleCebula/itemyZosi.py
@@ -1,4 +1,4 @@
-from productgetter import get_products_from_seller, get_products_from_category
+from productgetter import get_products_from_seller
 from propozycje.machineLearning.item_generator import categories_dict
 
 def get_total_price(bundle):
@@ -8,12 +8,11 @@ def get_total_price(bundle):
     return price
 
 
-def get_bundles(bundles, category_id, max_price, num_products):
+def get_bundles(bundles, seller_id, category_id, max_price, num_products):
     new_bundles=[]
-    new_items=get_products_from_category(cat_id=category_id, max_price=max_price, num_products=num_products)
+    new_items=get_products_from_seller(seller_id, cat_id=category_id, max_price=max_price, num_products=num_products)
     for item in new_items:
-       #if item["sellingMode"]["format"] == "BUY_NOW":
-       if True:
+        if item["sellingMode"]["format"] == "BUY_NOW":
             price=float(item["sellingMode"]["price"]["amount"])
             for bundle in bundles:
                 if get_total_price(bundle)+price<=max_price:
@@ -23,15 +22,13 @@ def get_bundles(bundles, category_id, max_price, num_products):
 
 
 def process_xd(items, categories, max_price, num_products):
-    bundles=[]
     for item in items:
-        print
-        #if item["sellingMode"]["format"] == "BUY_NOW":
-        #seller_id=item["seller"]["id"]
-        bundles.append([item])
-        for category in categories:
-            cat_id = categories_dict[category]
-            bundles=get_bundles(bundles, category, max_price, num_products)
+        if item["sellingMode"]["format"] == "BUY_NOW":
+            seller_id=item["seller"]["id"]
+            bundles=[[item]]
+            for category in categories:
+                cat_id = categories_dict[category]
+                bundles=get_bundles(bundles, seller_id, category, max_price, num_products)
     return bundles
 
 
@@ -39,7 +36,6 @@ def bundle_to_array(bundle):
     products=[]
     for item in bundle:
         products.append({
-            'id': item["id"],
             'name':item["name"],
             'price': item["sellingMode"]["price"]["amount"],
             'image':item["images"][0]["url"]
