b2b04b5991c2c7519478b822adbc33f687cbdc48
Zofia Tarant <zosiarrf@gmail.com>
2019-04-13 16:53:59 +0200

    product getter cleanup

--- alleCebula/alleCebula/productgetter.py
+++ alleCebula/alleCebula/productgetter.py
@@ -20,18 +20,6 @@ def get_products_from_category(cat_id, seller_id=None, max_price=None, num_produ
     return []
 
 
-def get_products_from_seller(seller_id, max_price=None, cat_id=None, num_products=None):
-    url = "https://api.allegro.pl/offers/listing?seller.id={}&sellingMode.format={}".format(str(cat_id), "BUY_NOW")
-    if cat_id:
-        url = url + "&category.id=" + str(cat_id)
-    if max_price:
-        url = url + '&price.from=0&price.to=' + str(max_price)
-    if num_products:
-        url = url + '&limit=' + str(num_products)
-    response = requests.get(url, headers={'Accept': 'application/vnd.allegro.public.v1+json',
-                                          'content-type': 'application/vnd.allegro.public.v1+json',
-                                          'Authorization': 'Bearer ' + token})
-
     if response.ok:
         return get_items_from_api(response.json())
     return []
