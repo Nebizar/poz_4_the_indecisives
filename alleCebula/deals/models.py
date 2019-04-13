a7a72ac1e9cec6a3877f59c7252b52f2f2e57b09
Krzysztof <krzysztof211@gmail.com>
2019-04-13 09:06:47 +0200

    comment class

--- alleCebula/deals/models.py
+++ alleCebula/deals/models.py
@@ -7,3 +7,8 @@ class Item(models.Model):
     price = models.CharField(max_length = 7)
     cebulions = models.IntegerField()
     category_id = models.CharField(max_length = 10)
+    
+class Comment(models.Model):
+    product_id = models.ForeignKey(Item, on_delete = models.CASCADE)
+    content = models.TextField()
+    nick = models.TextField()
