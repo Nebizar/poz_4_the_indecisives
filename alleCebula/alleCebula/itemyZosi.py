33936967427902e252de06d550a4713c2fc8c362
Krzysztof <krzysztof211@gmail.com>
2019-04-13 02:51:53 +0200

    git stuff solving

--- alleCebula/alleCebula/itemyZosi.py
+++ alleCebula/alleCebula/itemyZosi.py
@@ -1,4 +1,4 @@
-from productgetter import get_products_from_seller
+from alleCebula.productgetter import get_products_from_category
 from propozycje.machineLearning.item_generator import categories_dict
 
 def get_total_price(bundle):
@@ -8,9 +8,9 @@ def get_total_price(bundle):
     return price
 
 
-def get_bundles(bundles, seller_id, category_id, max_price, num_products):
+def get_bundles(bundles, category_id, max_price, num_products):
     new_bundles=[]
-    new_items=get_products_from_seller(seller_id, cat_id=category_id, max_price=max_price, num_products=num_products)
+    new_items=get_products_from_category(cat_id=category_id, max_price=max_price, num_products=num_products)
     for item in new_items:
         if item["sellingMode"]["format"] == "BUY_NOW":
             price=float(item["sellingMode"]["price"]["amount"])
@@ -22,23 +22,31 @@ def get_bundles(bundles, seller_id, category_id, max_price, num_products):
 
 
 def process_xd(items, categories, max_price, num_products):
+    bundles = []
     for item in items:
         if item["sellingMode"]["format"] == "BUY_NOW":
-            seller_id=item["seller"]["id"]
-            bundles=[[item]]
+            #seller_id=item["seller"]["id"]
+            bundles.append([item])
             for category in categories:
                 cat_id = categories_dict[category]
-                bundles=get_bundles(bundles, seller_id, category, max_price, num_products)
+                bundles=get_bundles(bundles, category, max_price, num_products)
     return bundles
 
 
 def bundle_to_array(bundle):
     products=[]
     for item in bundle:
-        products.append({
-            'name':item["name"],
-            'price': item["sellingMode"]["price"]["amount"],
-            'image':item["images"][0]["url"]
-        })
+        if(len(item["images"])>0):
+            products.append({
+                'name':item["name"],
+                'price': item["sellingMode"]["price"]["amount"],
+                'image':item["images"][0]["url"]
+            })
+        else:
+            products.append({
+                'name': item["name"],
+                'price': item["sellingMode"]["price"]["amount"],
+                'image': 'https://a.allegroimg.com/s1024/01100e/9c869ebe48129822b1605ecd4605'
+            })
     return products
 
--- alleCebula/propozycje/views.py
+++ alleCebula/propozycje/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from propozycje.machineLearning.rules import create_rules, get_associated_categories
 from alleCebula.productgetter import get_products_from_category, get_products_from_seller
 from propozycje.machineLearning.item_generator import categories_dict
+from alleCebula.itemyZosi import bundle_to_array, process_xd
 from django.http import HttpResponse
 from django.template import loader
 
@@ -23,13 +24,16 @@ def process(request, price, category):
     rules = create_rules()
     associated_categories= get_associated_categories(rules, category)
 
-    items_number = 100
+    items_number = 50
     category_number = len(associated_categories)
     items_per_category = items_number // category_number
 
     cat_id = categories_dict[category]
     items = get_products_from_category(cat_id, num_products=items_per_category, max_price=base_price)
     bundles=[]
+    products = []
+    bundles=process_xd(items, associated_categories, base_price, items_per_category)
+    print(bundles)
 
     for item in items:
         seller_id=item["seller"]["id"]
@@ -38,37 +42,40 @@ def process(request, price, category):
             other_price=base_price-float(item["sellingMode"]["price"]["amount"])
             for category in associated_categories:
                 cat_id=categories_dict[category]
-                other_items = get_products_from_seller(seller_id, max_price=other_price, num_products=items_per_category, cat_id=cat_id)
+                other_items = get_products_from_category(cat_id, max_price=other_price, num_products=items_per_category)
+
                 for other_item in other_items:
-                    bundle_sample = [item]
+                    bundle_sample = []
+                    bundle_sample.append(item)
                     if other_item["sellingMode"]["format"] == "BUY_NOW":
                         bundle_sample.append(other_item)
                         new_price = other_price - float(other_item["sellingMode"]["price"]["amount"])
                         bundles.append(bundle_sample)
+                        products = bundle_to_array(bundle_sample)
 
-        # get data from API and machine learning stuff
-        template = loader.get_template('propozycje/zero/productList.html')
 
-        products = [
-            {
-                'name': 'opona',
-                'price': '125 zł',
-                'image': 'https://a.allegroimg.com/s1024/01100e/9c869ebe48129822b1605ecd4605'
-            },
-            {
-                'name': 'opona',
-                'price': '125 zł',
-                'image': 'https://a.allegroimg.com/s1024/01100e/9c869ebe48129822b1605ecd4605'
-            },
-            {
-                'name': 'opona',
-                'price': '125 zł',
-                'image': 'https://a.allegroimg.com/s1024/01100e/9c869ebe48129822b1605ecd4605'
-            }
-        ]
+    template = loader.get_template('propozycje/zero/productList.html')
 
-        context = {
-            'products': products
+    """products = [
+        {
+            'name': 'opona',
+            'price': '125 zł',
+            'image': 'https://a.allegroimg.com/s1024/01100e/9c869ebe48129822b1605ecd4605'
+        },
+        {
+            'name': 'opona',
+            'price': '125 zł',
+            'image': 'https://a.allegroimg.com/s1024/01100e/9c869ebe48129822b1605ecd4605'
+        },
+        {
+            'name': 'opona',
+            'price': '125 zł',
+            'image': 'https://a.allegroimg.com/s1024/01100e/9c869ebe48129822b1605ecd4605'
         }
+    ]"""
 
-        return HttpResponse(template.render(context, request))
+    context = {
+        'products': products
+    }
+
+    return HttpResponse(template.render(context, request))
