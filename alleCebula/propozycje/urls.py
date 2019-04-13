0d15b1b05233963b8f66ea0ee62f1130f0a167ed
aieozn <aieozn@gmail.com>
2019-04-13 10:15:15 +0200

    'synch'

--- alleCebula/propozycje/urls.py
+++ alleCebula/propozycje/urls.py
@@ -5,5 +5,6 @@ from django.conf.urls import url
 urlpatterns = [
     path('', views.propositions, name='propositions'),
     url(r'^process/(?P<category>\w+)/(?P<price>\d+)/$', views.process, name='propositions-processing'),
+    url(r'^process/single/(?P<category>\d+)/(?P<price>\d+)/$', views.process, name='propositions-processing'),
     url(r'^buy/(?P<id>\w+)/$', views.buy, name='propositions-buy'),
 ]
