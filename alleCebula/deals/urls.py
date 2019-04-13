3cc597c562bd19aecf22e2bd5fef6d1f84cdf3ca
aieozn <aieozn@gmail.com>
2019-04-13 08:48:58 +0200

    fontend alleCebula

--- alleCebula/deals/urls.py
+++ alleCebula/deals/urls.py
@@ -5,5 +5,5 @@ from django.conf.urls import url
 urlpatterns = [
     path('', views.deals, name='deals'),
     #url(r'^process/(?P<category>\w+)/(?P<price>\d+)/$', views.process, name='propositions-processing'),
-    #url(r'^buy/(?P<id>\w+)/$', views.buy, name='propositions-buy'),
+    url(r'^(?P<id>\d+)/$', views.details, name='propositions-buy'),
 ]
--- alleCebula/deals/views.py
+++ alleCebula/deals/views.py
@@ -1,8 +1,36 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
-
+from deals.models import Item
 
 def deals(request):
-    template = loader.get_template('propozycje/zero/productList.html')
-    return HttpResponse(template.render(request))
+    template = loader.get_template('deals/index.html')
+    
+    """
+    pr = Item(
+        picture = "https://1.allegroimg.com/s720/0322e5/21cfc7684e40928e4445a5c74751",
+        name = "MYSZKA BEZPRZEWODOWA LOGITECH M545 CZARNA",
+        price = 95,
+        cebulions = "0",
+        category_id = "4575"
+    )
+    pr.save()"""
+
+    products = Item.objects.all()
+
+    context = {
+        'products': products
+    }
+    
+    return HttpResponse(template.render(context, request))
+
+def details(request, id):
+    template = loader.get_template('deals/singleSite.html')
+
+    product = Item.objects.get(pk=id)
+
+    context = {
+        'product': product
+    }
+
+    return HttpResponse(template.render(context, request))
